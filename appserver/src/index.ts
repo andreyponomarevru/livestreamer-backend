6f25b7a8ad5d77d47996122101e8fffc6d678442
Andrey Ponomarev <info@andreyponomarev.ru>
2021-05-16 04:30:33 +0300

    [chore] delete irrelevant comments

--- appserver/src/index.ts
+++ appserver/src/index.ts
@@ -9,7 +9,7 @@
 //       separate
 // TODO: send stream to clients-consumers through websocket
 
-// const express = require("express");
+import express from "express";
 import ws from "ws";
 import http from "http";
 import { v4 as uuidv4 } from "uuid";
@@ -18,15 +18,16 @@ import wav from "wav";
 const httpServer = http.createServer();
 const { PORT } = process.env;
 
+//
+
 function onRequest(req: any, res: any) {
   console.log(req.headers);
   console.log(`req.url: ${req.url}`);
 
   let filename = uuidv4(); // TODO: take filename from `uuidv4 || req.params.filename`
   const writableStream = fs.createWriteStream(`./streams/${filename}.wav`);
-  req.pipe(res); // FIX: send not to this response but on response for specific endpoint /stream
-
   //req.pipe(writableStream); // write sreeam to disk, temporary disabled
+  req.pipe(res); // FIX: send not to this response but on response for specific endpoint /stream
 
   req.on("data", (chunk: any) => {
     //console.log(chunk);
@@ -44,7 +45,6 @@ function onRequest(req: any, res: any) {
 
   req.on("close", () => {
     console.log("close event");
-    // TODO: put here the code that creates new .wav file for each connection, in order to avoid rewriting the file
     res.end();
   });
 }
@@ -52,17 +52,6 @@ function onRequest(req: any, res: any) {
 //
 
 httpServer.on("request", onRequest);
-
-/*
-function writeStreamToDisk() {
-  // outputFileStream is a writable stream
-  const outputFileStream = new wav.FileWriter("./stream.wav", {
-    channels: 2,
-    sampleRate: 48000, // the sample rate set here doesn't match the one shown in pulseaudio (pacmd list-sinks), otherwise the sound is slowed/pitched down
-    bitDepth: 16,
-  });
-}
-*/
 
 httpServer.listen(PORT, () => {
   console.log(`App HTTP server is listening on ${PORT}`);
