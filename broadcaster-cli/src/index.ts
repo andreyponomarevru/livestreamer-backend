fe3abf12bbff05ee66f0d7f747201534e6fe6ae6
Andrey Ponomarev <info@andreyponomarev.ru>
2021-08-27 18:44:43 +0300

    feat: Add broadcast client app

--- broadcaster-cli/src/index.ts
+++ broadcaster-cli/src/index.ts
@@ -5,9 +5,9 @@
 import { spawn } from "child_process";
 import http from "http";
 
-const postRequestOptions = {
+const reqOptions = {
   host: "localhost",
-  port: 8080,
+  port: 5000,
   path: "/stream",
   method: "POST",
   headers: {
@@ -19,24 +19,23 @@ const postRequestOptions = {
 //
 
 function onResponse(res: http.IncomingMessage) {
-  console.log(res.statusCode);
-  console.log(res.headers);
+  console.log(`${res.statusCode}\n${res.headers}`);
 
-  res.on("data", (chunk) => {
-    console.log(chunk);
-  });
-
+  res.on("data", (chunk) => console.log(chunk));
   res.on("error", (err) => {
     console.log(`Response error: ${err}`);
+    process.exit(1);
   });
 }
 
-function onRequestError(err: Error) {
+function onError(err: Error) {
   console.log(`Request error: ${err}`);
 }
 
-function onProcessExit(req: http.ClientRequest) {
-  req.end();
+function onProcessExit(this: http.ClientRequest) {
+  this.end();
+  console.log("Request ended.");
+  process.exit(0);
 }
 
 const child = spawn("ffmpeg", [
@@ -44,29 +43,27 @@ const child = spawn("ffmpeg", [
   "pulse",
   "-i", // input device "default"
   "default",
-  "-f", // output in .wav
-  "wav",
+  "-f", // output in .mp3
+  "mp3",
+  "-b:a", // set bitrate
+  "192k", // ":a" means "for audio"
   "pipe:1", // pipe instead of saving to disk
 ]);
 
-function sendPostRequest(options: http.RequestOptions) {
-  const req = http.request(options);
-  child.stdout.pipe(req);
+const req = http.request(reqOptions);
 
-  req.on("response", onResponse);
-  req.on("error", onRequestError);
-  process.on("exit", onProcessExit.bind(req));
-
-  child.stdout.on("end", () => {
-    req.end();
-    console.log("End stream");
-    console.log("Streaming stopped.");
-  });
-}
+req.on("response", onResponse);
+req.on("error", onError);
+process.on("SIGINT", onProcessExit.bind(req)); // intercept Ctrl+C
+process.on("uncaughtException", onProcessExit.bind(req));
+// NOTE: without piping to process.stderr,
+// ffmpeg will silently hang after a few minutes
+child.stderr.pipe(process.stderr);
+// Pass audio stream into request stream
+child.stdout.pipe(req);
 
 //
 
-sendPostRequest(postRequestOptions);
 console.log(
-  `HTTP Streaming Client is running on ${postRequestOptions.host}:${postRequestOptions.port}`,
+  `HTTP Client is streaming audio to ${reqOptions.host}:${reqOptions.port}\n`,
 );
