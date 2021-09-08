e5e1a69e5fe9476f76a97bb0bec913227d9c2d16
Andrey Ponomarev <info@andreyponomarev.ru>
2021-09-08 14:08:26 +0300

    feat: WS router ('upgrade' handler)

--- api/src/http-server-config.ts
+++ api/src/http-server-config.ts
@@ -3,9 +3,12 @@
 //
 
 import express from "express";
-import http from "http";
+import http, { IncomingMessage } from "http";
 import path from "path";
+import url from "url";
+import { Socket } from "net";
 
+import WebSocket from "ws";
 import morganLogger from "morgan";
 import { logger, morganSettings } from "./config/logger";
 import cors from "cors";
@@ -16,14 +19,46 @@ import {
 } from "./controllers/middlewares/error-handlers";
 import { onServerError } from "./event-handlers/error-handlers";
 import { router } from "./controllers/index";
-import { env } from "./config/env";
-import * as webSocketServer from "./ws-messaging-server-config";
+import { HTTP_PORT } from "./config/env";
+import * as wsInfoServer from "./ws-info-server-config";
+import * as wsStreamingServer from "./ws-streaming-server-config";
 import { onServerListening } from "./event-handlers/server-events";
+
+// Route WS requests
+function upgrade(req: IncomingMessage, socket: Socket, head: Buffer) {
+  const { pathname } = url.parse(req.url || "");
+
+  switch (pathname) {
+    /*
+		case "/info": {
+      // TODO: authenticate(...). Refer for Cookie auth example https://github.com/websockets/ws#client-authentication
+      console.log("WS Messaging Server: perform AuthN");
+      wsInfoServer.server.handleUpgrade(
+        req,
+        socket,
+        head,
+        wsInfoServer.handleAfterUpgrade,
+      );
+      break;
+    }*/
+    case "/stream": {
+      wsStreamingServer.server.handleUpgrade(
+        req,
+        socket,
+        head,
+        wsStreamingServer.handleAfterUpgrade,
+      );
+      break;
+    }
+    default:
+      socket.destroy();
+  }
+}
 
 // Express App
 
 const expressApp = express();
-expressApp.set("port", env.HTTP_PORT);
+expressApp.set("port", HTTP_PORT);
 
 expressApp.use(morganLogger("combined", morganSettings));
 expressApp.use(cors());
@@ -39,6 +74,6 @@ expressApp.use(expressCustomErrorHandler);
 const httpServer = http.createServer(expressApp);
 httpServer.on("error", onServerError);
 httpServer.on("listening", onServerListening);
-httpServer.on("upgrade", webSocketServer.upgrade);
+httpServer.on("upgrade", upgrade);
 
 export { httpServer };
