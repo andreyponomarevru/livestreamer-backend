946d3be00cada75cd0dc129a7336ddd95b1ef5f8
Andrey Ponomarev <info@andreyponomarev.ru>
2021-09-21 20:02:12 +0300

    fix: Remove unused modules; update 'import'

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,15 @@
 import fs from "fs-extra";
 import { v4 as uuidv4 } from "uuid";
-import wav from "wav";
 
 import { HTTP_PORT } from "./config/env";
-import { httpServer } from "./http-server-config";
+import { httpServer } from "./http-server";
+import { serverOptions } from "./config/ws-server";
 
 import {
   onUncaughtException,
   onUnhandledRejection,
   onWarning,
-} from "./event-handlers/error-handlers";
+} from "./event-handlers/process-handlers";
 
 process.once("uncaughtException", onUncaughtException);
 process.on("unhandledRejection", onUnhandledRejection);
