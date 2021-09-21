bec4c568c01490636da0c88eaa40f3ad2c3fe01d
Andrey Ponomarev <info@andreyponomarev.ru>
2021-09-21 20:00:51 +0300

    style: Update logger output

--- api/src/controllers/middlewares/validate.ts
+++ api/src/controllers/middlewares/validate.ts
@@ -5,15 +5,26 @@ import Joi from "joi";
 import { logger } from "../../config/logger";
 
 export function validate(
-  schema: Joi.ObjectSchema<any>,
+  schema:
+    | Joi.ObjectSchema<any>
+    | Joi.AlternativesSchema
+    | Joi.StringSchema
+    | Joi.NumberSchema
+    | Joi.ArraySchema,
   location: "body" | "headers" | "query" | "params",
 ) {
   return async function (req: Request, res: Response, next: NextFunction) {
+    logger.debug(
+      `${__filename}: [before validation] ${util.inspect(req[location])}`,
+    );
+
     try {
       const validated = await schema.validateAsync(req[location]);
       req[location] = { ...req[location], ...validated };
 
-      logger.debug(`${__filename}: ${util.inspect(req[location])}`);
+      logger.debug(
+        `${__filename}: [after validation] ${util.inspect(req[location])}`,
+      );
     } catch (err) {
       next(err);
     }
