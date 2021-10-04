165374b3d75349a75302fc51e7c9176e38068b3b
Andrey Ponomarev <info@andreyponomarev.ru>
2021-10-04 12:31:08 +0300

    feat: Implement stream service

--- api/src/services/stream/inout-stream.ts
+++ api/src/services/stream/inout-stream.ts
@@ -1,8 +1,24 @@
 import { Duplex, DuplexOptions } from "stream";
+import util from "util";
 
-export class InOutStream extends Duplex {
+import { BroadcastDraft } from "../../types";
+import { logger } from "../../config/logger";
+
+class InOutStream extends Duplex {
   constructor(options?: DuplexOptions) {
     super(options);
+
+    // By default, new streams are set to the 'flowing' mode. But as there is no need to use the stream right after the app's start up, we pause it. This will ease the subsequent management of the stream.
+    this.pause();
+  }
+
+  startStream(newBroadcast: BroadcastDraft) {
+    logger.debug(`${__filename}: [start] ${util.inspect(newBroadcast)}`);
+    this.emit("startstream", newBroadcast);
+  }
+
+  endStream() {
+    this.emit("endstream");
   }
 
   _read(size: number) {}
@@ -15,3 +31,9 @@ export class InOutStream extends Duplex {
     callback();
   }
 }
+
+const inoutStream = new InOutStream();
+// By default, new streams are set to the 'flowing' mode. But as there is no need to use the stream right after the app's start up, we pause it. This will ease the subsequent management of the stream.
+// inoutStream.pause(); // TODO: uncomment, it if this.pause() inside constructore above doesn't work
+
+export default inoutStream;
