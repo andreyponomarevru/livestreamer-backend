d7735d1a497eff4a8ffda4154184935c81c8f957
Andrey Ponomarev <info@andreyponomarev.ru>
2021-10-04 12:23:28 +0300

    feat: Add timestamps-related functions

--- api/src/utils/utils.ts
+++ api/src/utils/utils.ts
@@ -1,8 +1,10 @@
 import crypto from "crypto";
 import { Readable, Duplex } from "stream";
-
 import { v4 as uuidv4 } from "uuid";
+
 import bcrypt from "bcrypt";
+
+import { logger } from "../config/logger";
 
 import { WSSysMsg, WSUserMsg } from "../types";
 
@@ -27,6 +29,30 @@ export async function hashPassword(password: string, saltRounds = 10) {
   return await bcrypt.hash(password, salt);
 }
 
+export async function isPasswordMatch(password: string, hash: string) {
+  try {
+    return await bcrypt.compare(password, hash);
+  } catch (err) {
+    logger.error(`${__filename}: ${err}`);
+    return false;
+  }
+}
+
 export function generateToken(size = 64) {
   return crypto.randomBytes(size).toString("hex");
+}
+
+export const asciiRegex = new RegExp("^[\x00-\x7F]+$");
+
+export function getCurrentISOTimestampWithoutTimezone() {
+  return new Date(new Date().toUTCString()).toISOString();
+}
+
+export function encodeCursor(cursor: string) {
+  return Buffer.from(cursor).toString("base64");
+}
+
+export function decodeCursor(cursor?: string) {
+  if (!cursor) return [];
+  return Buffer.from(cursor, "base64").toString("ascii").split(",");
 }
