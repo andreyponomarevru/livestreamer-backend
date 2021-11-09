6c1b39f1a236129a8ac0106b250854862dd66806
Andrey Ponomarev <info@andreyponomarev.ru>
2021-11-09 13:38:52 +0300

    feat: Script to run different environments

---
+++ api/startapp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eu
+
+echo -ne "\n##\n##\tRUNNING WITH ENVIRONMENT=\"${NODE_ENV}\"\n##\n\n"
+
+if [ "${NODE_ENV}" == "test" ]
+then
+	npm run serve:test
+fi
+
+if [ "${NODE_ENV}" == "dev" ]
+then
+  npm run serve:dev
+fi
+
+if [ "${NODE_ENVENT}" == "prod" ]
+then
+	npm run build
+else
+  echo -ne "Invalid argument. Allowed values are: 'dev', 'test', 'prod'\n\n"
+fi
