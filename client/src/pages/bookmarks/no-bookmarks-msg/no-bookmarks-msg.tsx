33c847ccb4fe23bbe533604d28a53ac670e33457
Andrey Ponomarev <info@andreyponomarev.ru>
2021-11-09 14:36:03 +0300

    -

---
+++ client/src/pages/bookmarks/no-bookmarks-msg/no-bookmarks-msg.tsx
@@ -0,0 +1,24 @@
+import React, { ReactElement } from "react";
+import { NavLink } from "react-router-dom";
+
+import { Icon } from "../../../lib/icon/icon";
+import "../../lib/btn/btn.scss";
+import "../../lib/btn/btn.scss";
+
+import "./no-bookmarks-msg.scss";
+
+export function NoBookmarksMsg(): ReactElement {
+  return (
+    <div className="no-bookmarks-msg">
+      <p className="no-bookmarks-msg__main-text">Nothing saved yet :(</p>
+      <p className="no-bookmarks-msg__sub-text">
+        Tap <Icon iconName="bookmark" className="no-bookmarks-msg__icon" />
+        to add episodes to Bookmarks
+      </p>
+
+      <NavLink to="/archive" className="btn btn_theme_white no-bookmarks__btn">
+        Discover Episodes
+      </NavLink>
+    </div>
+  );
+}
