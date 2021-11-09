2af378ad78b0c474c62e5891caaa1062b2030fe1
Andrey Ponomarev <info@andreyponomarev.ru>
2021-11-09 13:37:28 +0300

    Setup hot reloading

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,22 +1,20 @@
-import React from "react";
+import React, { createContext } from "react";
 import { HashRouter } from "react-router-dom";
-
 import ReactDOM from "react-dom";
-import { hot } from "react-hot-loader/root";
-import { App } from "./components/app/app";
+
+import { App } from "./app/app";
+import { WebSocketProvider } from "./ws-client";
 
 // Global styles
 import "./reset.scss";
-import "./pages/page.scss";
-
-const rootEl = document.getElementById("root");
+import "./layout.scss";
 
 // TODO: replace HashRouter with BrowserRouter (https://reactrouter.com/web/guides/primary-components): keep everything as is, just just the router name in JSX tag. Than on Stackoverflow there is example of Nginx config that BrowserRouter needs to work correctly
 ReactDOM.render(
-  <HashRouter>
+  <WebSocketProvider>
     <App />
-  </HashRouter>,
-  rootEl
+  </WebSocketProvider>,
+  document.getElementById("root")
 );
 
-export default hot(App);
+//export default hot(App);
