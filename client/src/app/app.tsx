9ceefd65222710c029f074203853a08425268eb0
Andrey Ponomarev <info@andreyponomarev.ru>
2021-11-13 16:12:07 +0300

    fix: Replace the hardcoded routes

--- client/src/app/app.tsx
+++ client/src/app/app.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useContext } from "react";
+import React, { ReactElement } from "react";
 import {
   Route,
   BrowserRouter as Router,
@@ -16,7 +16,7 @@ import { PagesUsers } from "../pages/users/users";
 import { ForgotPassPage } from "../pages/forgot-pass/forgot-pass";
 import { PagesAccount } from "../pages/account-page/account-page";
 import { PagesAuth } from "../pages/auth-page/auth-page";
-import { ProtectedRoute } from "../lib/protected-route/protected-route";
+//import { ProtectedRoute } from "../lib/protected-route/protected-route";
 import { useAuthN, AuthNProvider } from "../hooks/use-authn";
 import { NavBar } from "../lib/nav-bar/nav-bar";
 import { StreamBar } from "../lib/stream-bar/stream-bar";
@@ -26,17 +26,12 @@ import { ConfirmRegistrationPage } from "../pages/confirm-registration/confirm-r
 import { PagesDrafts } from "../pages/drafts/drafts";
 import { WebSocketContext } from "../ws-client";
 import { useWSStreamState } from "../hooks/use-ws-stream-state";
+import { ROUTES } from "../config/routes";
 
 import "./app.scss";
 
-type Credentials = {
-  password: string;
-  username?: string;
-  email?: string;
-};
-
 export function App(): ReactElement {
-  const { user, updateUser, err, setErr, signin, signout } = useAuthN();
+  const { user } = useAuthN();
 
   return (
     <HashRouter>
@@ -51,23 +46,26 @@ export function App(): ReactElement {
           <main className="app__main">
             <Routes>
               <Route
-                path="/confirmation-required"
+                path={ROUTES.confirmationRequired}
                 element={<AskToConfirmRegistrationPage />}
               />
-              <Route path="/password-reset" element={<PassResetPage />} />
-              <Route path="/forgot-pass" element={<ForgotPassPage />} />
+              <Route path={ROUTES.passwordReset} element={<PassResetPage />} />
               <Route
-                path="/confirm-registration"
+                path={ROUTES.forgotPassword}
+                element={<ForgotPassPage />}
+              />
+              <Route
+                path={ROUTES.confirmRegistration}
                 element={<ConfirmRegistrationPage />}
               />
-              <Route path="/schedule" element={<PagesSchedule />} />
-              <Route path="/archive" element={<PagesArchive />} />
-              <Route path="/signin" element={<PagesAuth />} />
-              <Route path="/register" element={<PagesAuth />} />
-              <Route path="/drafts" element={<PagesDrafts />} />
-              <Route path="/account" element={<PagesAccount />} />
-              <Route path="/users" element={<PagesUsers />} />
-              <Route path="/" element={<PagesChat />} />
+              <Route path={ROUTES.schedule} element={<PagesSchedule />} />
+              <Route path={ROUTES.archive} element={<PagesArchive />} />
+              <Route path={ROUTES.signIn} element={<PagesAuth />} />
+              <Route path={ROUTES.register} element={<PagesAuth />} />
+              <Route path={ROUTES.drafts} element={<PagesDrafts />} />
+              <Route path={ROUTES.account} element={<PagesAccount />} />
+              <Route path={ROUTES.users} element={<PagesUsers />} />
+              <Route path={ROUTES.root} element={<PagesChat />} />
             </Routes>
           </main>
         </AuthNProvider>
