65afa3b31c7d1c9605a4061bc4856a74be0ff5c3
Andrey Ponomarev <info@andreyponomarev.ru>
2021-11-15 19:04:13 +0300

    refactor: Move button state to hook

--- client/src/pages/chat/heart-btn/heart-btn.tsx
+++ client/src/pages/chat/heart-btn/heart-btn.tsx
@@ -1,54 +1,28 @@
-import React, { ReactElement, useState, useContext, useEffect } from "react";
+import * as React from "react";
 
-import { API_ROOT_URL } from "../../../config/env";
-import { parseResponse } from "../../../utils/parse-response";
 import { useAuthN } from "../../../hooks/use-authn";
-import { useNavigate } from "react-router-dom";
 import { useIsMounted } from "../../../hooks/use-is-mounted";
 import { LIKE_TIMEOUT_MS } from "../../../config/env";
+import { useStreamLikeButton } from "../../../hooks/use-stream-like-button";
 
 import icons from "./../../../icons.svg";
 import "./heart-btn.scss";
-import { ROUTES } from "../../../config/routes";
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
   isStreamOnline: boolean;
 }
 
-export function HeartBtn(props: Props): ReactElement {
-  function handleBtnClick() {
-    if (!user) {
-      console.log(
-        "[heartBtn] To like the stream, the user has to be authenticated"
-      );
-      navigate(ROUTES.signIn);
-      return;
-    } else if (!props.isStreamOnline) {
-      console.log("[heartBtn] Stream is offline");
-      return;
-    }
-
-    const request: RequestInit = { method: "PUT" };
-    fetch(`${API_ROOT_URL}/stream/like`, request)
-      .then(parseResponse)
-      .then(() => {
-        setIsEnabled(false);
-      })
-      .catch((err) => console.log(err));
-  }
-
-  const navigate = useNavigate();
-  const { user } = useAuthN();
-  const [isEnabled, setIsEnabled] = useState(false);
-
+export function HeartBtn(props: Props): React.ReactElement {
   const isMounted = useIsMounted();
+  const auth = useAuthN();
+  const { handleBtnClick, setIsEnabled, isEnabled } = useStreamLikeButton();
 
-  useEffect(() => {
+  React.useEffect(() => {
     if (isMounted && props.isStreamOnline) setIsEnabled(true);
     else if (isMounted && !props.isStreamOnline) setIsEnabled(false);
-  }, [props.isStreamOnline, isMounted, user]);
+  }, [props.isStreamOnline, isMounted, auth.user]);
 
-  useEffect(() => {
+  React.useEffect(() => {
     let timerId: NodeJS.Timer;
     if (isMounted && !isEnabled && props.isStreamOnline) {
       timerId = setTimeout(() => setIsEnabled(true), LIKE_TIMEOUT_MS);
@@ -63,10 +37,10 @@ export function HeartBtn(props: Props): ReactElement {
   return (
     <button
       disabled={!isEnabled}
-      className={`heart-btn ${isEnabled ? "" : "heart-btn_disabled"} ${
-        props.className || ""
-      }`}
-      onClick={isEnabled ? handleBtnClick : undefined}
+      className={`heart-btn ${
+        isEnabled && props.isStreamOnline ? "" : "heart-btn_disabled"
+      } ${props.className || ""}`}
+      onClick={isEnabled && props.isStreamOnline ? handleBtnClick : undefined}
       type="submit"
       name="heart"
       value=""
