d9a4af1d6cc7967523f16749f990501767968397
Andrey Ponomarev <info@andreyponomarev.ru>
2021-11-25 10:39:59 +0300

    feat: Add config file

---
+++ src/config/api.ts
@@ -0,0 +1,9 @@
+export const API_HOST = "mix.ru";
+export const API_PORT = 8000;
+
+export const API_ROOT_PATH = "/api/v1";
+export const API_STREAM_PATH = "/stream";
+export const API_SESSION_URL = `http://${API_HOST}:${API_PORT}${API_ROOT_PATH}/sessions`;
+
+export const BROADCASTER_USERNAME = process.env.BROADCASTER_USERNAME;
+export const BROADCASTER_PASSWORD = process.env.BROADCASTER_PASSWORD;
