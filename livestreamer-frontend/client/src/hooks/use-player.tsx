34ebfbb5a60087ba13d77a219e44b098c94777c5
Andrey Ponomarev <info@andreyponomarev.ru>
2021-12-27 21:18:13 +0300

    fix: Reset 'src' on pause
    This fixes the major bug with player: previously, everytime the user
    clicks 'pause' button and then 'play' again, the player started playing
    from the very beginning of the stream instead of playing CURRENT audio
    stream (it lookes like the stream was cached).
    
    This commit fixes the issue: now, when you click play > pause
    > play, the stream is "reset" and you hear the same audio as all other
    users

--- livestreamer-frontend/client/src/hooks/use-player.tsx
+++ livestreamer-frontend/client/src/hooks/use-player.tsx
@@ -11,23 +11,32 @@ function usePlayer(url: string): Player {
   }
 
   React.useEffect(() => {
+    if (audio && isPlaying) audio.play();
+  }, [audio]);
+
+  React.useEffect(() => {
     if (isPlaying) {
-      if (!audio) setAudio(new Audio(url));
+      if (audio === null) {
+        setAudio(new Audio(url));
+      }
       const audioPromise = audio?.play();
       if (audioPromise !== undefined) {
         audioPromise.catch((err) => console.error(err));
       }
     } else {
       audio?.pause();
+      if (audio) {
+        // Without this block, after pressing the "play" button again, instead of picking up a current live audio stream, the player will play the old audio starting from the position when you first had clicked "play"
+        audio.removeAttribute("src");
+        audio.load();
+      }
       setAudio(null);
     }
-  }, [audio, isPlaying]);
-
-  React.useEffect(() => {
-    if (audio) audio.play();
-  }, [audio]);
+  }, [audio, isPlaying, url]);
 
   return { isPlaying, togglePlay };
 }
 
 export { usePlayer };
+
+//(audio && audio.readyState >= 3)
