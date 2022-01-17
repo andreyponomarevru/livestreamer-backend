70a715897a500f0704f06aedd5413b3ed44c8623
Andrey Ponomarev <info@andreyponomarev.ru>
2022-01-17 17:48:06 +0300

    refactor: clear interval only if it was set

--- livestreamer-backend/api/src/services/ws/scheduler.ts
+++ livestreamer-backend/api/src/services/ws/scheduler.ts
@@ -8,12 +8,13 @@ class IntervalScheduler implements Scheduler {
   timerId?: NodeJS.Timer;
 
   start(callback: () => void, interval: number): void {
-    console.log(`${__filename} Scheduler started`);
     this.timerId = setInterval(callback, interval);
   }
 
   stop(): void {
-    clearInterval(this.timerId as NodeJS.Timer);
+    if (!this.timerId) return;
+
+    clearInterval(this.timerId);
     this.timerId = undefined;
     console.log(
       `${__filename} Scheduler stopped, cleare timeerId ${this.timerId}`,
