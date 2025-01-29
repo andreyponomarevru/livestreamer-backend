8eee5245ef39eb33b76efc9ba5ba92291d29e88f
Andrey Ponomarev <info@andreyponomarev.ru>
2025-01-29 12:24:14 +0300

    Test ESLint and Prettier in CI

---
+++ api/eslint.config.mjs
@@ -0,0 +1,68 @@
+import prettier from "eslint-plugin-prettier";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ),
+  {
+    plugins: {
+      prettier,
+      "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.commonjs,
+        ...globals.node,
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+      },
+
+      parser: tsParser,
+      ecmaVersion: 2023,
+      sourceType: "module",
+    },
+
+    settings: {},
+
+    rules: {
+      "func-names": ["error", "never"],
+      "no-console": "off",
+      "prettier/prettier": ["error"],
+
+      "no-multiple-empty-lines": [
+        "error",
+        {
+          max: 4,
+        },
+      ],
+
+      "no-unused-vars": 1,
+
+      "no-warning-comments": [
+        0,
+        {
+          terms: ["TODO", "FIX", "todo", "fix"],
+          location: "anywhere",
+        },
+      ],
+    },
+  },
+];
