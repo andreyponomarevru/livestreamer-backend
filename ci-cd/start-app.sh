ff24f1a70ca92758ccc00bbcec9bca725db95e34
Andrey Ponomarev <info@andreyponomarev.ru>
2025-01-31 17:10:45 +0300

    49 ci add a new job for deployment to vps (#57)
    Add CD pipeline

--- ci-cd/start-app.sh
+++ ci-cd/start-app.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-# SCRIPT IS NOT TESTED
-
 set -eu -p pipefail
+
+cd ./test/livestreamer-backend
+git pull git@github.com:andreyponomarevru/livestreamer-backend.git
 
 printf "\n1. Pull images from Docker Hub and (re)starts app **************** \n"
 
-docker-compose --file "./docker-compose.prod.yml" pull --include-deps
+#docker-compose --file "./docker-compose.prod.yml" pull --include-deps
 
 printf "\n2. Restart the container ******************************************\n"
 
-docker-compose --file "./docker-compose.prod.yml" up \
-  --no-build \
-  --detach \
-  --force-recreate
+#docker-compose --file "./docker-compose.prod.yml" up \
+#  --no-build \
+#  --detach \
+#  --force-recreate
 
 prtinf "\n2. Delete old images **********************************************\n"
-docker image prune --force
+#docker image prune --force
 
 printf "\nThe app has been successfully (re)started! ************************\n"
