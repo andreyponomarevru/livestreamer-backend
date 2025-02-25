e071002be4718ee3c7bc1b21e534129bbfe99103
Andrey Ponomarev <info@andreyponomarev.ru>
2025-02-25 17:46:33 +0300

    Disable Winston logger during testing (#109)

--- api/src/config/logger.ts
+++ api/src/config/logger.ts
@@ -1,10 +1,10 @@
+import winston from "winston";
 import {
   APP_NAME,
   LOG_LOCATION,
   DEBUG_LOG_NAME,
   // INFO_LOG_NAME,
   // ERROR_LOG_NAME,
-  SHOULD_LOG_TO_CONSOLE,
   SHOULD_LOG_TO_FILE,
 } from "./../config/env";
 
@@ -12,51 +12,40 @@ import {
 // Winston logger
 //
 
-import winston from "winston";
-const {
-  createLogger,
-  format: { combine, timestamp, label, colorize, printf },
-  transports,
-} = winston;
+const logFormat = winston.format.printf(
+  ({ level, message, label, timestamp }) => {
+    return `${level} [${timestamp}] ${String(label).toUpperCase()}: ${message}`;
+  },
+);
 
-const logFormat = printf(({ level, message, label, timestamp }) => {
-  return `${level} [${timestamp}] ${String(label).toUpperCase()}: ${message}`;
-});
-
-const debugConsoleTransport = new transports.Console({
-  // Affect all logs with level 'debug' and below
-  level: "debug",
-  format: combine(
-    label({ label: APP_NAME }),
-    timestamp(),
-    colorize({ all: true }),
-    logFormat,
-  ),
-  silent: !SHOULD_LOG_TO_CONSOLE,
-});
-
-const debugFileTransport = new transports.File({
-  level: "debug",
-  filename: `${LOG_LOCATION}/${DEBUG_LOG_NAME}`,
-  format: combine(
-    label({ label: APP_NAME }),
-    timestamp(),
-    colorize({ all: true }),
-    logFormat,
-  ),
-  silent: !SHOULD_LOG_TO_FILE,
-});
-
-function createTransports(shouldLogToConsole = true, shouldLogToFile = true) {
-  const transports = [];
-  if (shouldLogToConsole) transports.push(debugConsoleTransport);
-  if (shouldLogToFile) transports.push(debugFileTransport);
-  return transports;
+function createTransports(shouldLogToFile = false) {
+  return shouldLogToFile
+    ? new winston.transports.File({
+        level: "debug",
+        filename: `${LOG_LOCATION}/${DEBUG_LOG_NAME}`,
+        format: winston.format.combine(
+          winston.format.label({ label: APP_NAME }),
+          winston.format.timestamp(),
+          winston.format.colorize({ all: true }),
+          logFormat,
+        ),
+      })
+    : new winston.transports.Console({
+        // Affect all logs with level 'debug' and below
+        level: "debug",
+        format: winston.format.combine(
+          winston.format.label({ label: APP_NAME }),
+          winston.format.timestamp(),
+          winston.format.colorize({ all: true }),
+          logFormat,
+        ),
+      });
 }
 
-const logger = createLogger({
-  transports: createTransports(SHOULD_LOG_TO_CONSOLE, SHOULD_LOG_TO_FILE),
+const logger = winston.createLogger({
+  transports: createTransports(SHOULD_LOG_TO_FILE),
   exitOnError: false,
+  silent: true,
 });
 
 // Put Morgan logs inside Winston logs,
@@ -64,11 +53,10 @@ const logger = createLogger({
 // function to get morgan-generated output into the winston log files
 const stream = {
   write: (message: string): void => {
-    // use the 'info' log level. The output will be picked up by both
-    // transports (file and console)
+    // use the 'info' log level. The output will be picked up by both 'file' and 'console' transports
     logger.info(message);
   },
-  silent: true,
+  silent: false,
 };
 
 //
