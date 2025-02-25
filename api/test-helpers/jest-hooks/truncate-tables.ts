29dbf4e90eddbaf31171563709d371c9a9bc4fff
Andrey Ponomarev <info@andreyponomarev.ru>
2025-02-25 17:39:33 +0300

    Disable Winston logger during testing

--- api/test-helpers/jest-hooks/truncate-tables.ts
+++ api/test-helpers/jest-hooks/truncate-tables.ts
@@ -1,6 +1,7 @@
 import { beforeEach } from "@jest/globals";
 import { dbConnection } from "../../src/config/postgres";
 import { redisConnection } from "../../src/config/redis";
+import { logger } from "../../src/config/logger";
 
 const tablesToTruncate = [
   "scheduled_broadcast",
@@ -11,13 +12,22 @@ const tablesToTruncate = [
   "chat_message",
   "appuser_setting",
   "appuser",
-].join(", ");
+];
 
-// Truncates tables before each test case
-beforeEach(async () => {
+async function truncateDBTables(tableNames: string[]) {
   const pool = await dbConnection.open();
-  await pool.query(`TRUNCATE ${tablesToTruncate} RESTART IDENTITY CASCADE`);
+  await pool.query(
+    `TRUNCATE ${tableNames.join(", ")} RESTART IDENTITY CASCADE`,
+  );
+}
 
+async function deleteAllRedisKeys() {
   const client = await redisConnection.open();
   await client.flushAll();
+}
+
+beforeEach(async () => {
+  await truncateDBTables(tablesToTruncate);
+  await deleteAllRedisKeys();
+  logger.silent = true;
 });
